46c48b36052ebd60e1b7b3555872841d1348d542
Pat Hickey <pat@moreproductive.org>
2010-03-15 23:00:55 -0400

    added ugliness block kludging

--- qkf.hs
+++ qkf.hs
@@ -1,13 +1,21 @@
 -- Quaternion Kalman Filter for attitude estimation
 -- Pat Hickey 15 Mar 10
 
-{-# LANGUAGE QuasiQuotes, NoMonomorphismRestriction #-}
+{-# LANGUAGE QuasiQuotes, NoMonomorphismRestriction, TypeOperators, FlexibleContexts 
+    , ViewPatterns, ScopedTypeVariables #-}
 -- 9 out of 10 computer scientists recommend hmatrix-static
 import Numeric.LinearAlgebra.Static
 import Data.Packed.Static.Imports
 
 type Quat = Vector D4 Double
 type AttitudeMat = Matrix (D3, D3) Double
+
+type MagCovMat = Matrix (D3, D3) Double
+type AccCovMat = Matrix (D3, D3) Double
+type GyroCovMat = Matrix (D3, D3) Double
+
+type BodyFQuat = Quat
+type RefFQuat = Quat
 
 qq :: Quat
 qq = [$vec| 0, 0, 0, 1|]
@@ -39,4 +47,26 @@ attMatOfQ qq = liftMatrix (*constant  (q*q - e <.> e)) (ident `atRows` d3) +
                liftMatrix (*constant (2*q)) (crossProdMat e)
                where q = qre qq 
                      e = qvec qq
-               
+
+--negM :: (IntegerT (m :*: n)) => Matrix (m,n) Double -> Matrix (m,n) Double
+--negM = liftMatrix (*constant (-1))
+
+vecQRightMat :: Vector D3 Double -> Matrix (D4, D4) Double
+vecQRightMat b = fromBlocks33to44 negBoX bo  negBot [$mat| 0 |]
+                 where negBoX = liftMatrix (*constant (-1)) (crossProdMat b)
+                       bo = asColumn b
+                       negBot = liftMatrix (*constant (-1)) (asRow b)
+
+-- Oh Dear God this is ugly:
+fromBlocks33to44 :: Matrix (D3,D3) Double -> Matrix (D3,D1) Double -> Matrix (D1,D3) Double -> Matrix (D1,D1) Double -> Matrix (D4, D4) Double
+fromBlocks33to44  (viewMat -> [$mat|a, b, c;
+                                    d, e, f;
+                                    g, h, i|])
+                  (viewMat -> [$mat|j; k; l|])
+                  (viewMat -> [$mat|m, n, o|])
+                  (viewMat -> [$mat|p|]) = [$mat|a, b, c, j;
+                                                  d, e, f, k;
+                                                  g, h, i, l;
+                                                  m, n, o, p|]
+
+
