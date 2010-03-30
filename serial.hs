90e2c93254afe61418e3f8e3afbf1045957d4ea2
Pat Hickey <pat@moreproductive.org>
2010-03-29 23:12:47 -0400

    redid Read to use Maybe instead of exceptions, implemented calibration display (ugly but it works)

--- serial.hs
+++ serial.hs
@@ -1,7 +1,7 @@
 -- Sensor frontend for Quaternion Kalman Filter
 -- Pat Hickey 17 Mar 10
 {-# LANGUAGE QuasiQuotes, NoMonomorphismRestriction, TypeOperators, FlexibleContexts 
-    , ViewPatterns, ScopedTypeVariables #-}
+    , ViewPatterns, ScopedTypeVariables, PatternGuards #-}
 module SerialQKF where
 import System.Hardware.Serialport
 import Control.Monad
@@ -12,7 +12,7 @@ import Qkf
 
 serialBegin :: IO SerialPort
 serialBegin = do
-  s <- openSerial "/dev/ttyUSB0" defaultSerialSettings { baudRate = B115200 }
+  s <- openSerial "/dev/ftdi5" defaultSerialSettings { baudRate = B115200 }
   -- next 3 lines: toggle reset on Arduino
   setDTR s False
   threadDelay 500000
@@ -35,20 +35,7 @@ defaultcov = [$mat|0.001, 0, 0;
                    0, 0.001, 0;
                    0, 0, 0.001|]
 
-data RawMeasurment =
-  RawMeasurment { rsource :: MeasurmentSource
-                , rx :: Double
-                , ry :: Double 
-                , rz :: Double } deriving (Show, Eq)
-
-rawOf :: MeasurmentSource -> [Char] -> Maybe RawMeasurment
-rawOf m s = let vs = split s ','
-            in case map read vs of
-              x:y:z:[] ->  Just RawMeasurment{ rsource = m
-                                             , rx = x
-                                             , ry = y
-                                             , rz = z }
-              _ -> Nothing
+data RawMeasurment = RM MeasurmentSource Double Double Double deriving (Show, Eq)
 
 split :: String -> Char -> [String]
 split [] _ = [""]
@@ -61,24 +48,61 @@ split (c:cs) delim
 parseLine :: [Char] -> Maybe RawMeasurment
 parseLine cs = let prefix = take 2 cs 
                in case prefix of
-               "$A" -> rawOf Accelerometer $ drop 3 cs
-               "$M" -> rawOf Magnetometer $ drop 3 cs
-               "$G" -> rawOf Gyro $ drop 3 cs
-               _ -> Nothing
+                 "$A" -> rawOf Accelerometer $ drop 3 cs
+                 "$M" -> rawOf Magnetometer $ drop 3 cs
+                 "$G" -> rawOf Gyro $ drop 3 cs
+                 _ -> Nothing
+                
 
+rawOf :: MeasurmentSource -> [Char] -> Maybe RawMeasurment
+rawOf m s = let vs = split s ',' in
+            case map readM vs of
+              (Just x):(Just y):(Just z):[] ->  Just (RM m x y z)
+              _ -> Nothing
+
+-- A safer way to 'read' using reads. Use Maybe to avoid Prelude.read's no parse exception
+readM :: (Read a) => String -> Maybe a
+readM s | x:[] <- parse = (Just x)
+        | otherwise = Nothing
+ where parse = [x | (x,_) <- reads s]
+
+{- unused, untested? ugly, at least.
 vecOfCsv :: [Char] -> Vector D3 Double
 vecOfCsv cs = let s = drop 3 cs
                   (x,y,z) = read s :: (Double,Double,Double)
                   h = (d1 >< d3) [ x, y, z ]
               in flatten h
+-}
 
+findminmax (mins, maxs) f = 
+  f (mins, maxs)                                                        >>= \r ->
+  let ((minacc, minmag),(maxacc, maxmag)) = r in
+  putStrLn ("acc- min: " ++ show minacc ++ " max: " ++ show maxacc ++
+    " -mag- min: " ++ show minmag ++ " max: " ++ show maxmag)           >> 
+  findminmax r f
 
-main :: IO ()
-main = do
+displaycalibration :: IO ()
+displaycalibration = do
   s <- serialBegin
-  forever $ do
-    l <- getLineMaybe s
-    case parseLine l of
-      Just a -> do { putStrLn (show a) }
-      Nothing -> return ()
+  let initmins = ((1024,1024,1024),(1024,1024,1024))
+  let initmaxs = ((-1024,-1024,-1024),(-1024,-1024,-1024))
+  findminmax (initmins, initmaxs) (minmaxlines s)
+  
+minmaxlines s (mins, maxs) = do
+  l <- getLineMaybe s
+  case parseLine l of
+    Just m@(RM Accelerometer _ _ _) -> 
+      let (accmin, accmax) = newminmax (fst mins, fst maxs) m in
+      return ((accmin, snd mins), (accmax, snd maxs))
+    Just m@(RM Magnetometer _ _ _) -> 
+      let (magmin, magmax) = newminmax (snd mins, snd maxs) m in
+      return ((fst mins, magmin), (fst maxs, magmax))
+    _ -> return (mins, maxs)
+
+newminmax ((minx, miny, minz),(maxx, maxy, maxz)) (RM _ x y z) = (mins', maxs')
+  where
+    mins' = (min minx x, min miny y, min minz z)
+    maxs' = (max maxx x, max maxy y, max maxz z)
+
+
 
