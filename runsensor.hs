d32264dd07a5710a90ff75c74f457456aad97a06
Pat Hickey <pat@moreproductive.org>
2010-04-06 00:16:23 -0400

    filter passed to cube with samplevar, loops with filterSamples to thread and report updated state

--- runsensor.hs
+++ runsensor.hs
@@ -2,31 +2,40 @@
 {-# LANGUAGE QuasiQuotes #-}
 import Numeric.LinearAlgebra.Static
 import Control.Concurrent
-import Control.Monad
 import Qkf
 import Cube
 import SerialQKF
 
+type CalSampleVar = SampleVar CalibratedMeasurment
+type ThreeCals = (CalSampleVar, CalSampleVar, CalSampleVar)
+type KFilter = (FilterState, RateEstimate)
+
 main = do
-  acccal <- newEmptySampleVar
-  magcal <- newEmptySampleVar
-  gyrocal <- newEmptySampleVar
-  forkIO $ sampleCalibrated (acccal, magcal, gyrocal)
-
-  fvar <- newEmptyMVar
-  putMVar fvar (fszero, rezero) 
-  forkIO $ forever $ filterSamples (acccal, magcal, gyrocal) fvar
+  acccal   <- newEmptySampleVar
+  magcal   <- newEmptySampleVar
+  gyrocal  <- newEmptySampleVar
+  filtered <- newEmptySampleVar
   
-  forkIO $ cubewith fvar
+  forkIO $ sampleCalibrated (acccal, magcal, gyrocal)
+  forkIO $ loopAndSend filtered (filterSamples (acccal, magcal, gyrocal)) (fszero, rezero)
+  forkIO $ cubewith filtered
+
+loopAndSend :: SampleVar a -> (a -> IO a) -> a -> IO ()
+loopAndSend svar f init =
+  let loop x = 
+        f x >>= \result ->
+        writeSampleVar svar result >>
+        loop result
+  in loop init
 
 
-filterSamples (acccal, magcal, gyrocal) fvar = 
+filterSamples :: ThreeCals -> KFilter ->  IO KFilter
+filterSamples (acccal, magcal, gyrocal) (fstate, rstate) = 
  let dt = 0.03 in
- forever $ do
+ do
   latestacc <- readSampleVar acccal
   latestmag <- readSampleVar magcal
   latestgyro <- readSampleVar gyrocal
-  (fstate, rstate) <- takeMVar fvar
   let acc = toMeasurment latestacc
   let mag = toMeasurment latestmag
   let gyro = toMeasurment latestgyro  
@@ -36,8 +45,7 @@ filterSamples (acccal, magcal, gyrocal) fvar =
   let fs'gyro = timePropogate rstate' fs'mag
   print fs'gyro
   print rstate'
-  putMVar fvar (fs'gyro, rstate')
-  threadDelay 50
+  return (fs'gyro, rstate')
 
 toMeasurment :: CalibratedMeasurment -> Measurment
 toMeasurment (CM Accelerometer x y z) =
