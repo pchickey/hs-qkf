e9dea976e180af4fb0867be226f1635e5006d93f
Pat Hickey <pat@moreproductive.org>
2010-04-06 22:49:29 -0400

    gnuplot works for a fixed number of samples

--- runsensor.hs
+++ runsensor.hs
@@ -9,32 +9,57 @@ import SerialQKF
 import Graphics.Gnuplot.Simple
 
 type CalSampleVar = SampleVar CalibratedMeasurment
-type ThreeCals = (CalSampleVar, CalSampleVar, CalSampleVar)
+type ThreeCalSamples = (CalSampleVar, CalSampleVar, CalSampleVar)
 type KFilter = (FilterState, RateEstimate)
+type ThreeMeas = (Measurment, Measurment, Measurment)
+type ThreeCals = (CalibratedMeasurment, CalibratedMeasurment, CalibratedMeasurment)
 
 main = do
   acccal   <- newEmptySampleVar
   magcal   <- newEmptySampleVar
   gyrocal  <- newEmptySampleVar
   filtered <- newEmptySampleVar
-  
+  reslog <- newChan
+
   t1 <- forkIO $ sampleCalibrated (acccal, magcal, gyrocal)
-  t2 <- forkIO $ loopAndSend filtered (filterSamples (acccal, magcal, gyrocal)) (fszero, rezero)
-  forkIO $ cubewith filtered >>= \results -> killThread t1 >> killThread t2 >> 
-  plotresults results
+  t2 <- forkIO $ loopAndSend filtered (filterSamples (acccal, magcal, gyrocal)) (fszero, rezero) reslog
+  t3 <- forkIO $ cubewith filtered >> killThread t1 >> killThread t2
+  
+  plotresults reslog
+  print "done plotting results"
 
-plotresults r =
+unzip3vec (v:vs) = (v1:v1s , v2:v2s, v3:v3s)
+  where v1 = v @> 0
+        v2 = v @> 1
+        v3 = v @> 2
+        (v1s, v2s, v3s) = unzip3vec vs
+unzip3vec [] = ([], [], [])
 
-loopAndSend :: SampleVar a -> (a -> IO a) -> a -> IO ()
-loopAndSend svar f init =
+plotresults rchan = do
+  print "getting chan contents"
+  rs <- getChanContents rchan 
+  let (outs, ins) = unzip $ take 100 rs
+  let (fstates, restates) = unzip outs
+  let (accs, mags, gyros) = unzip3 ins
+  let (axs, ays, azs) = unzip3vec (map body accs)
+  let (mxs, mys, mzs) = unzip3vec (map body mags)
+  let (gxs, gys, gzs) = unzip3vec (map body gyros)
+  print "beginning plots"
+  plotLists [Title "Accelerometer"] $ [axs, ays, azs]
+  plotLists [Title "Magnetometer"] [mxs, mys, mzs]
+
+
+-- loopAndSend :: SampleVar b -> (a -> IO a) -> a -> Chan a -> IO ()
+loopAndSend svar f init logchan =
   let loop x = 
         f x >>= \result ->
-        writeSampleVar svar result >>
-        loop result
+        writeSampleVar svar (fst result) >> -- now we're sending (outs, ins) as result
+        writeChan logchan result >>         -- forward outs to svar, log (outs, ins)
+        loop (fst result)
   in loop init
 
 
-filterSamples :: ThreeCals -> KFilter ->  IO KFilter
+filterSamples :: ThreeCalSamples -> KFilter ->  IO (KFilter, ThreeMeas)
 filterSamples (acccal, magcal, gyrocal) (fstate, rstate) = 
  let dt = 0.03 in
  do
@@ -58,7 +83,7 @@ filterSamples (acccal, magcal, gyrocal) (fstate, rstate) =
   --print rstate'
   --putStrLn $ "before gyro " ++ (show $ eulersOfQ $ q fs'mag)
   --putStrLn $ "after gyro  " ++ (show $ eulersOfQ $ q fs'gyro)
-  return (fs'gyro, rstate')
+  return ((fs'gyro, rstate'), (acc, mag, gyro))
 
 accMeasurment (CM Accelerometer x y z) =
   Measurment
